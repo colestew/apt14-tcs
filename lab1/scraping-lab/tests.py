400d559d68c6804b47c21fd4e4f194ade83c5011
colestewart <tcstewart09@gmail.com>
2014-11-03 10:32:13 -0600

    fixed scraping lab tests

---
+++ lab1/scraping-lab/tests.py
@@ -0,0 +1,32 @@
+import unittest
+import yahoo_options_data
+import json
+
+class ScrapingTest(unittest.TestCase):
+
+    def test_f(self):
+        computedJson = yahoo_options_data.contractAsJson("f.dat")
+        expectedJson = open("f.json").read()
+        expectedJson_change = open("f_change.json").read()
+        print computedJson
+        self.failUnless(json.loads(computedJson) == json.loads(expectedJson) \
+            or json.loads(computedJson) == json.loads(expectedJson_change))
+
+    def test_aapl(self):
+        computedJson = yahoo_options_data.contractAsJson("aapl.dat")
+        expectedJson = open("aapl.json").read()
+        expectedJson_change = open("aapl_change.json").read()
+
+        self.failUnless(json.loads(computedJson) == json.loads(expectedJson) \
+            or json.loads(computedJson) == json.loads(expectedJson_change))
+    
+    def test_xom(self):
+        computedJson = yahoo_options_data.contractAsJson("xom.dat")
+        expectedJson = open("xom.json").read()
+        expectedJson_change = open("xom_change.json").read()
+
+        self.failUnless(json.loads(computedJson) == json.loads(expectedJson) \
+            or json.loads(computedJson) == json.loads(expectedJson_change))
+    
+if __name__ == '__main__':
+    unittest.main()
